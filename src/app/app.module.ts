6b9bb0dda1b64ae53e04d1c3bd087e7fa93b9511
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-10-21 09:26:17 -0400

    wip: generate search and searchbox components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ResourceCardComponent } from './resource-card/resource-card.component';
+import { SearchComponent } from './search/search.component';
+import { SearchboxComponent } from './searchbox/searchbox.component';
 
 @NgModule({
-  declarations: [AppComponent, ResourceCardComponent],
+  declarations: [
+    AppComponent,
+    ResourceCardComponent,
+    SearchComponent,
+    SearchboxComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
