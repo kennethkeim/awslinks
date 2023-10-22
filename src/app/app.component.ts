4b1b2890bb2796e32986fbf96eda2089f37c8115
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-10-22 13:21:30 -0400

    wip: use tailwind classes on app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { getResources, mockConfig, mockResources } from './resources/resources';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   public readonly resources = getResources(mockResources, mockConfig);
