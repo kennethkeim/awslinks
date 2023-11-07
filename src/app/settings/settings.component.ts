bd0fa27bebe568238a613946baed6ba4de86dc4a
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-11-06 21:17:12 -0500

    wip: stupid reactive form workaround

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -10,4 +10,22 @@ export class SettingsComponent {
     resources: new FormControl(null, [Validators.required]),
     envs: new FormControl(null),
   });
+
+  public readonly textareaClasses = [
+    'bg-slate-50',
+    'border',
+    'border-slate-300',
+    'text-sm',
+    'rounded-md',
+    'focus:ring-blue-500',
+    'focus:border-blue-500',
+    'block',
+    'w-full',
+    'p-2.5',
+    'dark:bg-slate-700',
+    'dark:border-slate-600',
+    'dark:placeholder-slate-400',
+    'dark:focus:ring-blue-500',
+    'dark:focus:border-blue-500',
+  ];
 }
